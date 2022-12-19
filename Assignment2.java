cebdb5e768c7871280b923c31cadb1602e9b1976
yashrajrajpurohit <118617764+yashrajrajpurohit@users.noreply.github.com>
2022-12-20 00:53:12 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Assignment2 extends Assignment1 {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<n;i++) {
+			for(int j=1;j<n;j++) {
+			System.out.print(i);
+		}
+		
+		System.out.println(" ");
+		// TODO Auto-generated method stub
+
+	}
+	}
+}
---
+++ Assignment3.java
@@ -0,0 +1,22 @@
+package variables;
+
+public class Assignment3 {
+	public static void main(String args[]) {
+		int n=15;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(j==0 &&i<=(n-1)/2||
+						i+j<=(n-1)/2||
+						j-i>=(n-1)/2||
+						j==n-1 && i<=(n-1)/2){
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
